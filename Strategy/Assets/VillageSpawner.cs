8802cb92cf76e3b3265277c378412f8dd83ee8b7
Luc Somers <lucsomers@hotmail.com>
2020-09-23 00:05:54 +0200

    Added village spawning and more trees

---
+++ Strategy/Assets/VillageSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageSpawner : MonoBehaviour
+{
+    [SerializeField] private float SpawnTimer = 0;
+    [SerializeField] private GameObject Soldier;
+
+    private float currentTimeOnTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimeOnTimer += Time.deltaTime;
+
+        if (currentTimeOnTimer >= SpawnTimer)
+        {
+            currentTimeOnTimer = 0;
+            SpawnNewSoldier();
+        }
+    }
+
+    private void SpawnNewSoldier()
+    {
+        GameObject tempSoldier = Instantiate(Soldier);
+
+        tempSoldier.transform.position = transform.position;
+    }
+}
